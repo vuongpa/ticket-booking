959e86f616f4dc862708f0f65121db9e9af9e3bb
vuongpa <phananhvuong200015@gmail.com>
2025-05-11 20:26:20 +0700

    First worked version

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,31 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
-import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    ClientsModule.registerAsync([
-      {
-        name: 'USERS_SERVICE',
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.GRPC,
-          options: {
-            package: 'user',
-            protoPath: join(
-              __dirname,
-              '../../../apps/users/src/infrastructure/proto/user.proto',
-            ),
-            url: `${configService.get('USERS_SERVICE_HOST', 'localhost')}:${configService.get('USERS_SERVICE_PORT', 50051)}`,
-          },
-        }),
-        inject: [ConfigService],
-      },
-    ]),
     UsersModule,
   ],
 })
--- apps/gateway/src/users/users.module.ts
+++ apps/gateway/src/users/users.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersController } from './users.controller';
+import { ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -8,6 +11,23 @@ import { UsersController } from './users.controller';
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1d' },
     }),
+    ClientsModule.registerAsync([
+      {
+        name: 'USERS_SERVICE',
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.GRPC,
+          options: {
+            package: 'user',
+            protoPath: join(
+              __dirname,
+              '../../../apps/users/src/infrastructure/proto/user.proto',
+            ),
+            url: `${configService.get('USERS_SERVICE_HOST', 'localhost')}:${configService.get('USERS_SERVICE_PORT', 50051)}`,
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
   ],
   controllers: [UsersController],
 })
